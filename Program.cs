f13e17bb29bca38fe14b60d63b2454f809b68986
Guilherme Almeida <guilhermedealmeidasilva17@gmail.com>
2024-06-20 14:07:54 -0300

    finalizado

---
+++ Program.cs
@@ -0,0 +1,62 @@
+﻿decimal valorSaque, valorRestante = 1.2M;
+int  nota1, nota2, nota5, nota10, nota20, nota50, nota100, nota200;
+ 
+Console.ForegroundColor = ConsoleColor.DarkBlue;
+Console.WriteLine(".....Caixa Eletrônico.....\n");
+Console.ResetColor();
+ 
+Console.ForegroundColor = ConsoleColor.Yellow;
+Console.Write("----- Valor de Saque -----:R$");
+valorSaque = Convert.ToDecimal(Console.ReadLine());
+Console.ResetColor();
+ 
+if  (valorSaque <=0)
+{
+Console.ForegroundColor = ConsoleColor.DarkRed;
+Console.Write("Valor não permitido para saque\n");
+return;
+}
+Console.ResetColor();
+ 
+ 
+valorRestante = Math.Round(valorRestante,0);
+ 
+valorRestante = valorSaque;
+ 
+nota200 = (int)(valorRestante / 200);
+valorRestante = valorRestante % 200;
+ 
+nota100 = (int)(valorRestante / 100);
+valorRestante = valorRestante % 100;
+ 
+nota50 = (int)(valorRestante / 50);
+valorRestante = valorRestante % 50;
+ 
+nota20 = (int)(valorRestante / 20);
+valorRestante = valorRestante % 20;
+ 
+nota10 = (int)(valorRestante / 10);
+valorRestante = valorRestante % 10;
+ 
+nota5 = (int)(valorRestante / 5);
+valorRestante = valorRestante % 5;
+ 
+nota2 = (int)(valorRestante / 2);
+valorRestante = valorRestante % 2;
+ 
+nota1 = (int)(valorRestante / 1);
+valorRestante = valorRestante % 1;
+ 
+Console.ForegroundColor = ConsoleColor.Green;
+Console.WriteLine ("Nota(s) para entregar:\n");
+ 
+if (nota1 > 0) Console.WriteLine($"{nota1} nota(s) de R$1,00");
+if (nota2 > 0) Console.WriteLine($"{nota2} nota(s) de R$2,00");
+if (nota5 > 0) Console.WriteLine($"{nota5} nota(s) de R$5,00");
+if (nota10 > 0) Console.WriteLine($"{nota10} nota(s) de R$10,00");
+if (nota20 > 0) Console.WriteLine($"{nota20} nota(s) de R$20,00");
+if (nota50 > 0) Console.WriteLine($"{nota50} nota(s) de R$50,00");
+if (nota100 > 0) Console.WriteLine($"{nota100} nota(s) de R$100,00");
+if (nota200 > 0) Console.WriteLine($"{nota200} nota(s) de R$200,00");
+ 
+Console.ResetColor();
--- README.md
+++ README.md
@@ -1,2 +1,45 @@
-# CaixaEletronico
-Projeto Final 1ML3 do Curso de Informática ETEC Adolpho Berezin 2024.
+# Caixa Eletrônico
+Software sobre exercício de Projeto Final (Caixa Eletrônico) 1ML3 de informática ETEC Adolpho Berezin.
+ 
+Programa consiste em realizar um saque em um caixa eletrônico que calcula a quantidade de notas a entregar ao solicitante dado um valor desejado para saque (o número deve ser positivo e não pode conter centavos), calcule a quantidade de cada nota a ser entregue. Dê preferência para notas de valor mais alto.
+ 
+Notas disponíveis: 1, 2, 5, 10, 20, 50, 100 e 200 reais
+ 
+## Exemplo
+ 
+```
+.....Caixa Eletrônico.....
+ 
+----- Valor de Saque -----:R$10
+Nota(s) para entregar:
+ 
+1 nota(s) de R$10,00
+```
+ 
+## _Screenshot_
+ 
+![Programa sendo executado](Imagens/image.png)
+ 
+## _Download_
+ 
+Download do arquivo abaixo:
+ 
+[🗃 Download do arquivo .zip]
+ 
+Para executar, utilize o comando:
+ 
+```
+comando para executar
+```
+ 
+Ou, se preferir, pode dar duplo-clique no ícone do programa.
+ 
+## Agradecimentos
+ 
+ 
+- [Etec Adolpho Berezin](https://eteab.com.br)
+- [Prof. Ermogenes](https://github.com/ermogenes)
+- [Prof. Diego Neri](https://github.com/diegoneri)
+ 
+ 
+Todos direitos reservados aos criadores®™
